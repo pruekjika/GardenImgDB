6603bd51b3ea7e7b19dba9ea40da3b0832fbd1a4
meangpu <pruekjika@gmail.com>
2023-12-28 06:11:29 +0700

    init python scpt

--- Python/timer.py
+++ Python/timer.py
@@ -1,9 +1,10 @@
-#python3
+# python3
 # from [How do I get time of a Python program's execution? - Stack Overflow](https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution)
 import atexit
 from time import time, strftime, localtime
 from datetime import timedelta
 from log_setup import logger
+
 
 def secondsToStr(elapsed=None):
     if elapsed is None:
@@ -11,8 +12,9 @@ def secondsToStr(elapsed=None):
     else:
         return str(timedelta(seconds=elapsed))
 
+
 def log(s, elapsed=None):
-    line = "="*40
+    line = "=" * 40
     logger.info(line)
     word = f"{secondsToStr()} {'-'} {s}"
     logger.info(word)
@@ -20,12 +22,13 @@ def log(s, elapsed=None):
         logger.info(f"Elapsed time: {elapsed}")
     logger.info(line)
 
-def endlog():
+
+def end_log():
     end = time()
-    elapsed = end-start
+    elapsed = end - start
     log("End Program", secondsToStr(elapsed))
-    
+
 
 start = time()
-atexit.register(endlog)
+atexit.register(end_log)
 log("Start Program")
