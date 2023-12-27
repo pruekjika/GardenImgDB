898998e002c8f548f4d39979867996d4424ee745
meangpu <51825872+meangpu@users.noreply.github.com>
2023-12-27 18:18:30 +0700

    update package

--- Web/src/App.tsx
+++ Web/src/App.tsx
@@ -1,19 +1,32 @@
 import "./App.css";
-import firstImage from "../../ImageDB/Fixed/__0.jpg";
-import secondImage from "../../ImageDB/Fixed/__1.jpg";
+import firstImage from "../../ImageDB/Fixed/__1.jpg";
+import secondImage from "../../ImageDB/Fixed/__19.jpg";
+
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from "react-compare-slider";
 
 function App() {
   return (
     <>
       <div className='container'>
-        <h3>Image compare</h3>
-        <div className='img-comp-container'>
-          <div className='img-comp-img'>
-            <img src={firstImage} width='1000' height='700' />
-          </div>
-          <div className='img-comp-img img-comp-overlay'>
-            <img src={secondImage} width='1000' height='700' />
-          </div>
+        <div>
+          <ReactCompareSlider
+            boundsPadding={0}
+            itemOne={
+              <ReactCompareSliderImage alt='Image one' src={firstImage} />
+            }
+            itemTwo={
+              <ReactCompareSliderImage alt='Image two' src={secondImage} />
+            }
+            keyboardIncrement='5%'
+            position={50}
+            style={{
+              height: "100vh",
+              width: "100%",
+            }}
+          />
         </div>
       </div>
     </>
