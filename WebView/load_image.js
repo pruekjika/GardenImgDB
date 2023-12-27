2106ea5d98d8cc489224cca65e4330f123fc237a
meangpu <51825872+meangpu@users.noreply.github.com>
2023-12-27 16:16:30 +0700

    start write html

---
+++ WebView/load_image.js
@@ -0,0 +1,14 @@
+var folder = "ImageDB/Fixed/";
+
+$.ajax({
+  url: folder,
+  success: function (data) {
+    $(data)
+      .find("a")
+      .attr("href", function (i, val) {
+        if (val.match(/\.(jpe?g|png|gif)$/)) {
+          $("body").append("<img src='" + val + "'>");
+        }
+      });
+  },
+});
