1e51463deb58f7fe29208730be397d0c558d3762
meangpu <51825872+meangpu@users.noreply.github.com>
2024-05-08 20:48:44 +0700

    update

--- PythonScpt/main.py
+++ PythonScpt/main.py
@@ -21,7 +21,7 @@ img_original_count = count_image_file(gp(img_original_path), "*.jpg")
 ###############################################################################
 # change below
 ALLOW_PERCENT = 0.01
-KEYPOINT = 28000
+KEYPOINT = 19400
 DELETE_OLD_IMG = True
 # end need to change
 ###############################################################################
